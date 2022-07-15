5f9f662b326c816666b23a2c2739141c93a33c24
Hanfie Vandanu <hanfiev@gmail.com>
2022-07-15 10:13:26 -0400

    single project page update

--- maps/projects/app.js
+++ maps/projects/app.js
@@ -1,12 +1,12 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuZmlldmFuZGFudSIsImEiOiJjbDQ3aWdoN2EwaW93M2tyM2xqejFxNjhnIn0.GdmoXUuInsJ_i2OmR9StRg';
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9, // starting zoom
-projection: 'globe' // display the map as a 3D globe
-});
+// mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuZmlldmFuZGFudSIsImEiOiJjbDQ3aWdoN2EwaW93M2tyM2xqejFxNjhnIn0.GdmoXUuInsJ_i2OmR9StRg';
+// const map = new mapboxgl.Map({
+// container: 'map', // container ID
+// style: 'mapbox://styles/mapbox/streets-v11', // style URL
+// center: [-74.5, 40], // starting position [lng, lat]
+// zoom: 9, // starting zoom
+// projection: 'globe' // display the map as a 3D globe
+// });
  
-map.on('style.load', () => {
-map.setFog({}); // Set the default atmosphere style
-});
+// map.on('style.load', () => {
+// map.setFog({}); // Set the default atmosphere style
+// });
