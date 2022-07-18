35e7b00c9703e98c681cb7bd74150ec972b04324
Hanfie Vandanu <hanfiev@gmail.com>
2022-07-18 17:14:24 -0400

    update

--- maps/app.js
+++ maps/app.js
@@ -1,10 +1,16 @@
 // initialize map
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGFuZmlldmFuZGFudSIsImEiOiJjbDQ3aWdoN2EwaW93M2tyM2xqejFxNjhnIn0.GdmoXUuInsJ_i2OmR9StRg';
 const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/light-v10', // style URL
-  center: [-73.935242, 40.730610], // starting position [lng, lat]
-  zoom: 11 // starting zoom
+container: 'map', // container ID
+style: 'mapbox://styles/hanfievandanu/cl5n7thfo001716l6wa2mpe2p', // style URL
+center: [-73.976,40.735], // starting position [lng, lat]
+zoom: 11, // starting zoom
+pitch: 0,
+projection: 'globe' // display the map as a 3D globe
+});
+ 
+map.on('style.load', () => {
+map.setFog({}); // Set the default atmosphere style
 });
 
 ZAPdata = []
